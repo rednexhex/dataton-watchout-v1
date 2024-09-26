1480c86545aff55eec084df895c12dc5a30185eb
rednexhex <rednexhex@gmail.com>
2024-09-26 01:54:35 -0500

    commit2

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "Matty~Dataton~Watchout",
   Version = "1.0",
-  BuildVersion = "2.3.1.68",
+  BuildVersion = "2.3.1.69",
   Id ="57fb018e-2917-4d28-8ab4-7f65396816fb",
   Author = "Matty",
   Description = "TCP/Telnet Control of Watchout Software"
